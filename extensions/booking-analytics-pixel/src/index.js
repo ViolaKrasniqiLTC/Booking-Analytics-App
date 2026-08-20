a30c3838c4d987b05b52e550222fb0e0ca05be4d
ronakukaj1 <rona.kukaj@lowerthecurve.com>
2026-08-20 11:20:18 +0200

    added other extension files to the branch

--- extensions/booking-analytics-pixel/src/index.js
+++ extensions/booking-analytics-pixel/src/index.js
@@ -1,4 +1,8 @@
 import { register } from "@shopify/web-pixels-extension";
 import { registerAddToCart } from "./addToCart";
+import { setupPageViewTracking } from "./pageView";
 
-register(registerAddToCart);
+register((ctx) => {
+  registerAddToCart(ctx);
+  setupPageViewTracking(ctx.analytics, ctx.init, ctx.settings);
+});
--- extensions/booking-analytics-pixel/src/pageView.js
+++ extensions/booking-analytics-pixel/src/pageView.js
@@ -1,15 +1,5 @@
-import { register } from '@shopify/web-pixels-extension';
-
-register(({ analytics }) => {
-  analytics.subscribe('page_viewed', (event) => {
-    console.log('====================');
-    console.log('PAGE VIEWED!');
-    console.log('Event name:', event.name);
-    console.log('Event ID:', event.id);
-    console.log('Timestamp:', event.timestamp);
-    console.log('Client ID:', event.clientId);
-    console.log('Page URL:', event.context.document.location.href);
-    console.log('Full event:', event);
-    console.log('====================');
+export function setupPageViewTracking(analytics) {
+  analytics.subscribe("page_viewed", (event) => {
+    console.log("Page viewed", event);
   });
-});
+}
